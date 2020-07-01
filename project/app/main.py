e3942a63fa2943d2f042d3aac315259f73e82b0b
ngjulia <jen67@cornell.edu>
2020-07-01 01:56:29 -0400

    finished pytest setup

--- project/app/main.py
+++ project/app/main.py
@@ -1,7 +1,9 @@
 # project/app/main.py
 
+import os
 
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
@@ -9,10 +11,19 @@ from app.config import get_settings, Settings
 app = FastAPI()
 
 
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
-  return {
-    "ping": "pong!",
-    "environment": settings.environment,
-    "testing": settings.testing
-}
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
